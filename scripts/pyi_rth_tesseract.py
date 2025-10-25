25b18572e1c7c06538e33fc2b8d3b16a96514302
a <a@a.com>
2025-10-25 18:33:07 +0200

    Include tessaract hook for pyinstaller

---
+++ scripts/pyi_rth_tesseract.py
@@ -0,0 +1,12 @@
+
+import sys
+import os
+from pathlib import Path
+
+if getattr(sys, 'frozen', False):
+    # If the application is run as a bundle, the PyInstaller bootloader
+    # extends the sys module by a flag frozen=True and sets the app 
+    # path into variable _MEIPASS'.
+    application_path = Path(sys._MEIPASS)
+    tesseract_path = application_path
+    os.environ['PATH'] = str(tesseract_path) + os.pathsep + os.environ['PATH']
